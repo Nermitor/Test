07ae709c89dd6ca80ad091e649112032c575088b
dmaty <dmatyukhin2006@mail.ru>
2025-12-25 07:42:13 +0700

    Добавлены стили и автоимпорты

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
-
-<template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<template lang="pug">
+el-container.app-layout
+  el-header
+    | Header
+  el-main
+    router-view
+  el-footer
+    | Footer
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-layout {
+  min-height: 100vh;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/reset.css'
+import './styles/main.css'
+
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
 app.use(router)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
